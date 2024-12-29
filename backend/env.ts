53e65e8e332a57a2d0dcdaca107e5e9b2a398636
FelipeHeilmann <felipeheilmannm@gmail.com>
2024-12-29 00:48:16 -0300

    feat: add cors remove github url from env

--- backend/env.ts
+++ backend/env.ts
@@ -5,7 +5,6 @@ const envSchema = z.object({
     ENV: z.union([z.literal('development'), z.literal('production')]).default('development'),
     GITHUB_CLIENT_ID: z.string(),
     GITHUB_CLIENT_SECRET: z.string(),
-    GITHUB_ACCESS_TOKEN_URL: z.string(),
     LOG_LEVEL: z.string()
 })
 
