7a6c31c74e93232464852e15cf3d688be39e6f3f
Guilherme China <echina725@gmail.com>
2025-01-19 21:56:59 -0300

    test action && lint push changes

--- server/__test__/UserService.spec.ts
+++ server/__test__/UserService.spec.ts
@@ -1,5 +1,3 @@
-import { IGithubApiGateway } from "../src/application/gateway/IGithubApiGateway";
-import { IGithubWebGateway } from "../src/application/gateway/IGithubWebGateway";
 import { UserService } from "../src/application/service/UserService";
 import { ILogger } from "../src/infra/config/ILogger";
 import { GithubApiGateway } from "../src/infra/gateway/GithubApiGateway";
@@ -7,29 +5,51 @@ import { GithubWebGateway } from "../src/infra/gateway/GithubWebGateway";
 import { mock } from "jest-mock-extended";
 import { AuthCredentials } from "../src/utils/types/githubUser";
 
-let userService: UserService;
-const mockLogger = mock<ILogger>();
-const mockGithubApi = mock<GithubApiGateway>();
-const mockGithubWeb = mock<GithubWebGateway>();
+describe("UserService", () => {
+  let userService: UserService;
+  const mockLogger = mock<ILogger>();
+  const mockGithubApi = mock<GithubApiGateway>();
+  const mockGithubWeb = mock<GithubWebGateway>();
 
-beforeEach(() => {
-  jest.clearAllMocks();
-  userService = new UserService(mockLogger, mockGithubWeb, mockGithubApi);
-});
+  beforeEach(() => {
+    jest.clearAllMocks();
+    userService = new UserService(mockLogger, mockGithubWeb, mockGithubApi);
+  });
 
-it("should auth user using oauth", async () => {
-  const code = "authCode";
-  const mockResponse: AuthCredentials = {
-    accessToken: "token123",
-    refreshToken: "refresh123",
-  };
+  it("should auth user using oauth", async () => {
+    const code = "authCode";
+    const mockResponse: AuthCredentials = {
+      accessToken: "token123",
+      refreshToken: "refresh123",
+    };
 
-  mockGithubWeb.oauth.mockResolvedValueOnce(mockResponse);
+    mockGithubWeb.oauth.mockResolvedValueOnce(mockResponse);
 
-  const result = await userService.oauth(code);
+    const result = await userService.oauth(code);
 
-  expect(result).toEqual(mockResponse);
-  expect(mockLogger.log).toHaveBeenCalledWith(
-    "Requesting Github Gateway for user authentication",
-  );
+    expect(mockGithubWeb.oauth).toHaveBeenCalledWith(code);
+    expect(result).toEqual(mockResponse);
+    expect(mockLogger.log).toHaveBeenCalledWith(
+      "Requesting Github Gateway for user authentication",
+    );
+  });
+
+  it("should refresh token", async () => {
+    const refreshToken = "refreshToken123";
+    const mockResponse: AuthCredentials = {
+      accessToken: "newToken",
+      refreshToken: "newRefresh",
+    };
+
+    mockGithubWeb.refreshToken.mockResolvedValueOnce(mockResponse);
+
+    const result = await userService.refresh(refreshToken);
+
+    expect(result).toEqual(mockResponse);
+    expect(mockGithubWeb.refreshToken).toHaveBeenCalledWith(refreshToken);
+
+    expect(mockLogger.log).toHaveBeenCalledWith(
+      "Requesting Github Gateway for user authentication with refresh token",
+    );
+  });
 });
