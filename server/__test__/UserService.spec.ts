33d4032cbe28981f29996aa9e8635a67b00e10a9
Guilherme China <echina725@gmail.com>
2025-01-20 12:04:03 -0300

    test && rm yarn.lock

--- server/__test__/UserService.spec.ts
+++ server/__test__/UserService.spec.ts
@@ -3,7 +3,7 @@ import { ILogger } from "../src/infra/config/ILogger";
 import { GithubApiGateway } from "../src/infra/gateway/GithubApiGateway";
 import { GithubWebGateway } from "../src/infra/gateway/GithubWebGateway";
 import { mock } from "jest-mock-extended";
-import { AuthCredentials } from "../src/utils/types/githubUser";
+import { AuthCredentials, GithubUser } from "../src/utils/types/githubUser";
 
 describe("UserService", () => {
   let userService: UserService;
@@ -51,5 +51,44 @@ describe("UserService", () => {
     expect(mockLogger.log).toHaveBeenCalledWith(
       "Requesting Github Gateway for user authentication with refresh token",
     );
+  });
+
+  it("should load authorized user info", async () => {
+    const code = "authCode";
+    const mockedUserInfo: GithubUser = mock<GithubUser>();
+
+    mockGithubApi.fetchAuthorizedUserInfo.mockResolvedValueOnce(mockedUserInfo);
+
+    const result = await userService.loadAuthorizedUserInfo(code);
+
+    expect(result).toEqual(mockedUserInfo);
+    expect(mockLogger.log).toHaveBeenCalledWith(
+      "Requesting Github Gateway information about for authorized user",
+    );
+  });
+
+  it("should throw on load specific user", async () => {
+    const code = "authCode";
+    const username = "invalidUser";
+    mockGithubApi.fetchSpecificUser.mockRejectedValueOnce(
+      "Failed to get specific user",
+    );
+
+    await expect(userService.loadSpecificUser(code, username)).rejects.toThrow(
+      "Failed to get specific user",
+    );
+  });
+
+  it("should load specific user", async () => {
+    const token = "authCode";
+    const username = "username";
+    const mockedUser = mock<GithubUser>();
+
+    mockGithubApi.fetchSpecificUser.mockResolvedValue(mockedUser);
+
+    const result = await userService.loadSpecificUser(token, username);
+
+    expect(result).toEqual(mockedUser);
+    expect(mockLogger.error).not.toHaveBeenCalledTimes(1);
   });
 });
